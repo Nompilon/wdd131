570ce6bf3a619a1dd9c07d4e1f52f8f5d03c1ec2
Nompilon <ngwenyanompilo@gmail.com>
2025-05-18 20:26:25 +0200

    bom project done

---
+++ week02/scripts/bom.js
@@ -0,0 +1,41 @@
+//declare three (3) variables that hold references to the input, button, and list elements
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+//Create a click event listener for the Add Chapter button using an addEventListener.
+button.addEventListener('click', function () {
+    // Code to execute when the element is clicked
+
+    if (input.value.trim() !== '') {
+
+        //Create a li element that will hold each entry's chapter title and an associated delete button
+        const li = document.createElement('li');
+        //Create a delete button
+        const deleteButton = document.createElement('button');
+        //Populate the li element variable's textContent or innerHTML with the input value
+        li.textContent = input.value;
+        //Populate the button textContent with a ❌
+        deleteButton.textContent = '❌';
+        deleteButton.style.marginLeft = '10px';
+        //Append the li element variable with the delete button
+        li.append(deleteButton);
+        //Append the li element variable to the unordered list in your HTML
+        list.append(li);
+        
+        // Add delete functionality
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+       
+    } else {
+        input.value = '';
+        input.focus();
+
+    }
+});
+
